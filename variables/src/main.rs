0e633009e3c5d08f09521bbd910d2fa44a1db31a
Caleb Trachte <cetrachte@hotmail.com>
2020-07-22 15:44:06 -0500

    add const example

--- variables/src/main.rs
+++ variables/src/main.rs
@@ -4,6 +4,9 @@ fn main() {
     println!("The value of x is: {}", x);
     x = 6;
     println!("The value of x is: {}", x);
+    const SPEED_OF_LIGHT: u32 = 299_792_458;
+    println!("The value of the speed of light is: {} m/s", SPEED_OF_LIGHT);
+    // CONSTANTS are always immutable, and the type must be annotated.
 }
 //In Rust, the compiler guarantees that when you state that a value won’t change, it really won’t change.
 // That means that when you’re reading and writing code, you don’t have to keep track of how and where a value might change.
