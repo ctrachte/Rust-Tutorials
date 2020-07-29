af6adc0251ded2dcc51f5a9ba0760f3b309a36ef
Caleb Trachte <cetrachte@hotmail.com>
2020-07-29 14:07:43 -0500

    add if/else example

--- variables/src/main.rs
+++ variables/src/main.rs
@@ -34,16 +34,25 @@
 // }
 
 // Destructuring a tuple:
+// fn main() {
+
+//     //The variable tup binds to the entire tuple, because a tuple is considered a single compound element.
+//     // To get the individual values out of a tuple, we can use pattern matching to destructure a tuple value, like this:
+//     let tup: (i32, f64, u8) = (500, 6.4, 1);
+
+//     let (x, y, z) = tup;
+
+//     println!("The value of x is: {}", x);
+//     println!("The value of y is: {}", y);
+//     println!("The value of z is: {}", z);
+
+// }
+
+// If/Else example:
 fn main() {
+    let condition = true;
+    let number = if condition { 5 } else { 6 };
+    // let number = if condition { 5 } else { "six" }; // not allowed
 
-    //The variable tup binds to the entire tuple, because a tuple is considered a single compound element.
-    // To get the individual values out of a tuple, we can use pattern matching to destructure a tuple value, like this:
-    let tup: (i32, f64, u8) = (500, 6.4, 1);
-
-    let (x, y, z) = tup;
-
-    println!("The value of x is: {}", x);
-    println!("The value of y is: {}", y);
-    println!("The value of z is: {}", z);
-
+    println!("The value of number is: {}", number);
 }
