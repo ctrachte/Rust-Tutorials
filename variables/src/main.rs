241c22278a3dc71d7c873cf17bd2220b5ddcd552
Caleb Trachte <cetrachte@hotmail.com>
2020-08-05 11:34:10 -0500

    update return type example

--- variables/src/main.rs
+++ variables/src/main.rs
@@ -78,9 +78,13 @@
 fn five() -> i32 {
     5
 }
-
+fn plus_one(x: i32) -> i32 {
+    x + 1
+}
 fn main() {
     let x = five();
-
+    let five_plus_one = plus_one(x);
     println!("The value of x is: {}", x);
+    println!("The value of x plus one is: {}", five_plus_one);
+
 }
